3111927460f4e0ce86c685bcc3ab022c9c50096e
countdracular <lizikuan96@163.com>
2015-09-04 22:33:51 +0800

    refine routes for map page.

--- app.js
+++ app.js
@@ -70,6 +70,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/map', map);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
