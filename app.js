a6f78ab650b9da32ff971978f331c26295ea33b7
countdracular <lizikuan96@163.com>
2015-09-05 23:32:03 +0800

    trivials

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use('/', wechat(config, function (req, res, next) {
     // 回复高富帅(图文回复)
     res.reply([
       {
-        title: "You said: " + message.content.toString(),
+        title: "You said: " + message.toString(),
         description: 'hello world',
         picurl: "http://img3.douban.com/lpic/s9117507.jpg",
         url: "http://52.27.77.52/wxjssdktest/"
